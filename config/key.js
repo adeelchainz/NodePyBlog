2d11766a0e8067a34ae3d5c6226f783f07657d5d
Adeel <onnexdevelopment@gmail.com>
2020-03-06 16:12:21 +0500

    Private info to dev.js

---
+++ config/key.js
@@ -0,0 +1,6 @@
+//Switch the env
+if(process.env.NODE_ENV === 'production'){
+    module.exports= require('./prod');
+} else {
+    module.exports= require('./dev');
+}
--- index.js
+++ index.js
@@ -2,16 +2,37 @@
 const express= require('express');
 const app= express();
 const mongoose= require('mongoose');
+const bodyParser= require('body-parser');
+const cookieParser= require('cookie-parser');
+
+//Imports
+const {User}= require('./model/user');
+const config= require('./config/key'); 
 
 //Connecting db
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect('mongodb+srv://adeelchainz:Manamiz-123@nodeblog-kwwve.mongodb.net/test?retryWrites=true&w=majority',
+mongoose.connect(config.mongoURI,
     {useNewUrlParser: true}).then(()=>console.log('Connected')).catch(err=>console.error(err));
 
 //Routes, Simple Server
 app.get('/', (req,res)=>{
-    res.send('Hello!')
+    res.send('Hello! Adeel')
 });
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//Routes
+app.post('/api/user/register', (req,res)=> {
+    const user= new User(req.body)
+    user.save((err,userData)=> {
+        if(err) return res.json({success: false, err})
+    })
+    return res.status(200).json({
+        success: true
+    })
+})
 
 
 //Assigning Port, Standard 5000 for Node
